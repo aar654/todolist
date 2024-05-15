07e4ee10d4a3d0e4abc7a567331614a0b564a070
Aaron Smith <smithaaron654@gmail.com>
2024-05-14 20:40:00 -0400

    adding functionality for buttons

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import './style.css';
 const allTasks = document.getElementById("allTasks");
 const today = document.getElementById("today");
 const thisWeek = document.getElementById("thisWeek");
-const addProject = document.getElementById("addProject")
-const projects = document.getElementById("projects")
+const addProject = document.getElementById("addProject");
+const projectList = document.getElementById("projectList");
+const sidebarButton = document.querySelectorAll(".sidebarButton");
+let headerMain = document.getElementById("headerMain");
 
-let projectArr = []
+let projects = [];
 
 class Project {
     constructor(title) {
@@ -17,6 +19,15 @@ class Project {
     addTasks(task) {
         this.tasks.push(task);
     }
+
+    removeTask(taskTitle) {
+        this.tasks = this.tasks.filter(task => task.title !== taskTitle);
+    }
+
+    getTasks() {
+        return this.tasks;
+    }
+
 }
 
 class Task {
@@ -31,22 +42,49 @@ class Task {
 
 addProject.addEventListener("submit", function (e) {
     e.preventDefault();
-    const projectButton = document.createElement("button");    
-    const img = document.createElement('img');
-    const x = document.createElement('img');
-    img.src = "/dist/img/list.png"
-    x.src = "/dist/img/close.png"
-    x.classList.add("sidebarX")
-    img.classList.add("sidebarImg")
+
+    const projectButton = document.createElement("button");
+    const img = document.createElement("img");
+    const x = document.createElement("img");
+    let projectTitle = document.createElement("span");
+    img.src = "/dist/img/list.png";
+    x.src = "/dist/img/close.png";
+    x.classList.add("sidebarX");
+    img.classList.add("sidebarImg");
     projectButton.classList.add("btn", "btn-light", "sidebarButton");
-    let projectTitle = document.getElementById('addProjectValue').value;
-    projectButton.innerHTML = projectTitle;
-    projectButton.appendChild(img)
-    projectButton.appendChild(x)
-    projects.appendChild(projectButton)
-})
+    projectTitle.textContent = document.getElementById('addProjectValue').value;
+    projectButton.appendChild(img);
+    projectButton.appendChild(projectTitle)
+    projectButton.appendChild(x);
+    projectList.appendChild(projectButton);
+    projectButton.addEventListener("click", function(){
+        displayProjects();
+
+    })
+
+    let newProject = new Project(document.getElementById('addProjectValue').value);
+    projects.push(newProject);
+
+    document.getElementById('addProjectValue').value = "";
+    
+    console.log(projects);
+
+});
+
+for (let i = 0; i < sidebarButton.length; i++) {
+    sidebarButton[i].addEventListener("click", function () {
+        displayProjects();
+    })
+}
+
+let exampleProject = new Project("Example Project");
+let task1 = new Task("Example", "Example Description", "12/23/2024", true)
+exampleProject.addTasks(task1);
+projects.push(exampleProject);
+
+console.log(projects)
 
 
-
-
-
+function displayProjects() {
+    headerMain.innerHTML("hello");
+}
