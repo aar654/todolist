3432a78f863c97b93cc637ff53fa9c178c849cb8
Aaron Smith <smithaaron654@gmail.com>
2024-06-04 16:17:26 -0400

    created function to add task to project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './style.css';
-import { format, parseISO } from "date-fns";
-
+import {isToday, isThisWeek} from "date-fns";
 
 const allTasks = document.getElementById("allTasks");
 const today = document.getElementById("today");
@@ -17,7 +16,7 @@ let projects = [];
 class Project {
     constructor(title) {
         this.title = title;
-        this.tasks = []
+        this.tasks = [];
     }
 
     addTasks(task) {
@@ -25,13 +24,12 @@ class Project {
     }
 
     removeTask(taskTitle) {
-        this.tasks = this.tasks.filter(task => task.title !== taskTitle);
+        this.tasks = this.tasks.filter(task => task.task !== taskTitle);
     }
 
     getTasks() {
         return this.tasks;
     }
-
 }
 
 class Task {
@@ -39,10 +37,9 @@ class Task {
         this.task = task;
         this.taskDescription = taskDescription;
         this.dueDate = dueDate;
-        this.check = check
+        this.check = check;
     }
 }
-
 
 addProject.addEventListener("submit", function (e) {
     e.preventDefault();
@@ -58,7 +55,7 @@ addProject.addEventListener("submit", function (e) {
     projectButton.classList.add("btn", "btn-light", "sidebarButton", "projectButton");
     projectTitle.textContent = document.getElementById('addProjectValue').value;
     projectButton.appendChild(img);
-    projectButton.appendChild(projectTitle)
+    projectButton.appendChild(projectTitle);
     projectButton.appendChild(x);
     projectList.appendChild(projectButton);
 
@@ -74,69 +71,88 @@ addProject.addEventListener("submit", function (e) {
     console.log(projects);
 });
 
-
 let exampleProject = new Project("Example Project");
-let task1 = new Task("Example Task Title", "Example Description", "05/28/2024", false)
+let task1 = new Task("Example Task Title", "Example Description", "2024-05-28", false);
 exampleProject.addTasks(task1);
 projects.push(exampleProject);
 
-console.log(projects)
+console.log(projects);
 
 addTask.addEventListener("submit", function (e) {
     e.preventDefault();
 
-    let projectTasks = document.getElementById("projectTasks");
+    let currentProjectTitle = mainHeader.innerText;
 
-    const left = document.createElement("div");
-    const right = document.createElement("div");
-    left.classList.add("marginLeft");
-    right.classList.add("marginRight");
-    const taskButton = document.createElement("button");
-    taskButton.classList.add("btn", "btn-light", "projectButton", "projectContainer")
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.classList.add("form-check-input");
-    const taskTitle = document.createElement("span");
-    const taskDescription = document.createElement("span");
-    const taskDate = document.createElement("input");
+    let currentProject = projects.find(project => project.title === currentProjectTitle);
 
-    taskTitle.textContent = document.getElementById('taskTitle').value;
-    taskDescription.textContent = `(${document.getElementById('taskDescription').value})`;
-    taskDate.value = document.getElementById('taskDate').value;
+    if (currentProject) {
+        let newTask = new Task(
+            document.getElementById('taskTitle').value,
+            document.getElementById('taskDescription').value,
+            document.getElementById('taskDate').value,
+            false
+        );
 
-    //add task to array, put language that will create a new class task and add to the specific project created
+        currentProject.addTasks(newTask);
 
-    taskDate.type = "date";
-    const x = document.createElement("img");
-    x.src = "/dist/img/close.png";
-    x.classList.add("projectX")
-    taskDescription.classList.add("description");
+        displayProjectTasks(currentProject.getTasks());
 
-    left.appendChild(checkbox);
-    left.appendChild(taskTitle);
-    left.appendChild(taskDescription);
-    right.appendChild(taskDate);
-    right.appendChild(x);
+        document.getElementById('taskTitle').value = "";
+        document.getElementById('taskDescription').value = "";
+        document.getElementById('taskDate').value = "";
 
-    taskButton.appendChild(left);
-    taskButton.appendChild(right);
-
-    projectTasks.appendChild(taskButton);
-
-    document.getElementById('taskTitle').value = "";
-    document.getElementById('taskDescription').value = ""
-    document.getElementById('taskDate').value = ""
-
-    document.getElementById('taskModal').classList.remove('show');
-    document.getElementById('taskModal').setAttribute('aria-hidden', 'true');
-    document.body.classList.remove('modal-open');
-    document.querySelector('.modal-backdrop').remove();
-
-    console.log(projects);
-
-    e.preventDefault();
-
+        document.getElementById('taskModal').classList.remove('show');
+        document.getElementById('taskModal').setAttribute('aria-hidden', 'true');
+        document.body.classList.remove('modal-open');
+        document.querySelector('.modal-backdrop').remove();
+    }
 });
+
+function displayProjectTasks(tasks) {
+    const projectTasks = document.getElementById("projectTasks");
+    projectTasks.innerHTML = "";
+
+    tasks.forEach(task => {
+        const left = document.createElement("div");
+        const right = document.createElement("div");
+        left.classList.add("marginLeft");
+        right.classList.add("marginRight");
+
+        const taskButton = document.createElement("button");
+        taskButton.classList.add("btn", "btn-light", "projectContainer");
+
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.classList.add("form-check-input");
+        checkbox.checked = task.check;
+
+        const taskTitle = document.createElement("span");
+        taskTitle.textContent = task.task;
+
+        const taskDescription = document.createElement("span");
+        taskDescription.textContent = `(${task.taskDescription})`;
+        taskDescription.classList.add("description");
+
+        const taskDate = document.createElement("input");
+        taskDate.type = "date";
+        taskDate.value = task.dueDate;
+
+        const x = document.createElement("img");
+        x.src = "/dist/img/close.png";
+        x.classList.add("projectX");
+
+        left.appendChild(checkbox);
+        left.appendChild(taskTitle);
+        left.appendChild(taskDescription);
+        right.appendChild(taskDate);
+        right.appendChild(x);
+
+        taskButton.appendChild(left);
+        taskButton.appendChild(right);
+
+        projectTasks.appendChild(taskButton);
+    });
+}
 
 function displayProject() {
     let projectButton = document.querySelectorAll(".projectButton");
@@ -147,56 +163,48 @@ function displayProject() {
             if (index !== undefined && projects[index]) {
                 const project = projects[index];
                 mainHeader.innerHTML = project.title;
-                const projectTasks = document.getElementById("projectTasks");
-
-                projectTasks.innerHTML = "";
-
-                project.getTasks().forEach(task => {
-                    const left = document.createElement("div");
-                    const right = document.createElement("div");
-                    left.classList.add("marginLeft");
-                    right.classList.add("marginRight");
-
-                    const taskButton = document.createElement("button");
-                    taskButton.classList.add("btn", "btn-light", "projectContainer");
-
-                    const checkbox = document.createElement("input");
-                    checkbox.type = "checkbox";
-                    checkbox.classList.add("form-check-input");
-                    checkbox.checked = task.check;
-
-                    const taskTitle = document.createElement("span");
-                    taskTitle.textContent = task.task;
-
-                    const taskDescription = document.createElement("span");
-                    taskDescription.textContent = `(${task.taskDescription})`;
-                    taskDescription.classList.add("description");
-
-                    const taskDate = document.createElement("input");
-                    taskDate.type = "date";
-                    taskDate.value = task.dueDate;
-
-                    const x = document.createElement("img");
-                    x.src = "/dist/img/close.png";
-                    x.classList.add("projectX");
-
-                    left.appendChild(checkbox);
-                    left.appendChild(taskTitle);
-                    left.appendChild(taskDescription);
-                    right.appendChild(taskDate);
-                    right.appendChild(x);
-
-                    taskButton.appendChild(left);
-                    taskButton.appendChild(right);
-
-                    projectTasks.appendChild(taskButton);
-                });
-
+                displayProjectTasks(project.getTasks());
             } else {
                 mainHeader.innerHTML = "No Project Found";
             }
         });
     });
 }
+
+allTasks.addEventListener("click", function (){
+    mainHeader.innerHTML = "All Tasks";
+
+    let allTasksList = [];
+    projects.forEach(project => {
+        allTasksList = allTasksList.concat(project.getTasks());
+    });
+
+    displayProjectTasks(allTasksList);
+});
+
+today.addEventListener("click", function (){
+    mainHeader.innerHTML = "Today";
+
+    let todayTasks = [];
+
+    projects.forEach(project => {
+        todayTasks = todayTasks.concat(project.getTasks().filter(task => isToday(task.dueDate)));
+    });
+
+    displayProjectTasks(todayTasks);
+
+});
+
+thisWeek.addEventListener("click", function (){
+    mainHeader.innerHTML = "This Week";
+
+    let thisWeekTasks = [];
+
+    projects.forEach(project => {
+        thisWeekTasks = thisWeekTasks.concat(project.getTasks().filter(task => isThisWeek(task.dueDate)));
+    });
+
+    displayProjectTasks(thisWeekTasks);
+});
 
 displayProject();
